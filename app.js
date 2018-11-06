51dfcd7c175a61db7a69e721f990bb3afed8a21d
eunodevteam <devteam@euno.co>
2018-11-06 21:34:30 +0000

    Enable Cross-Origin Resource Sharing

--- app.js
+++ app.js
@@ -47,6 +47,14 @@ var express = require('express'),
 
 var app = express();
 
+// enable CORS
+//
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // bitcoinapi
 bitcoinapi.setWalletDetails(settings.wallet);
 
