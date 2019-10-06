64d9a43eb9b310cb8ded5db55ebed4ac74a1e36b
arjanvanderwal <arjanvdwal1992@gmail.com>
2019-10-06 15:04:40 +0200

    -- Small PM2 fix

--- pm2.euno.explorer.js
+++ pm2.euno.explorer.js
@@ -2,7 +2,7 @@ module.exports = {
     apps : [
         {
             name: "euno_explorer",
-            script: "npm run start",
+            script: "node --stack-size=10000 ./bin/cluster",
             watch: false,
             env: {
                 "NODE_ENV": "production"
